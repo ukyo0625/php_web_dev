580250fbfaa33708b1f0f20a7c4af89b1e8429ed
kobayashiukyo <u.k.knua0419@gmail.com>
2020-10-06 17:53:53 +0900

    add_header_footer

--- work/web/index.php
+++ work/web/index.php
@@ -2,32 +2,12 @@
 
 require('../app/functions.php');
 
-
-$names = [
-  // 'Taro',
-  // 'Jiro',
-  // 'Saburo',
-];
-
+include('../app/_parts/_header.php')
 ?>
 
 
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <title>PHP Practice</title>
-</head>
-<body>
-  <ul>
 
-  <?php if (empty($names)): ?>
-    <li>Nobody!</li>
-  <?php else : ?>
-  <?php foreach ($names as $name): ?>
-    <li><?= h($name); ?></li>
-  <?php endforeach; ?>
-  <?php endif; ?>
-  </ul>
-</body>
-</html>
+  
+<?php
+
+include('../app/_parts/_footer.php')
