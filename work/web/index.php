84787ccce9748fd1c1d472c52c431b4c3473b99d
kobayashiukyo <u.k.knua0419@gmail.com>
2020-10-07 18:41:09 +0900

    add_trim

--- work/web/index.php
+++ work/web/index.php
@@ -7,7 +7,6 @@ include('../app/_parts/_header.php')
 
 <form action="result.php" method="get">
   <input type="text" name="message">
-  <input type="text" name="username">
   <button>Send</button>
 </form>
 
--- work/web/result.php
+++ work/web/result.php
@@ -2,13 +2,13 @@
 
 require('../app/functions.php');
 
-$message = filter_input(INPUT_GET, 'message');
-$username = filter_input(INPUT_GET, 'username');
-
+$message = trim(filter_input(INPUT_GET, 'message'));
+$message = $message !== '' ? $message : '...';
+ 
 include('../app/_parts/_header.php')
 ?>
 
-<p><?= h($message); ?> by <?= h($username); ?></p>
+<p><?= h($message); ?></p>
 <p><a href="index.php">Go back</a></p>
 
 <?php
