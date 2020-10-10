ce68c5ecee5ca1c49b2ef30239b3b663a18dc87a
kobayashiukyo <u.k.knua0419@gmail.com>
2020-10-10 20:11:03 +0900

    add_token

--- work/web/index.php
+++ work/web/index.php
@@ -2,9 +2,13 @@
 
 require('../app/functions.php');
 
+createToken();
+
 define('FILENAME', '../app/messages.txt');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+validateToken();
+
   $message = trim(filter_input(INPUT_POST, 'message'));
   $message = $message !== '' ? $message : '...';
 
@@ -30,6 +34,7 @@ include('../app/_parts/_header.php')
 <form action="" method="post">
   <input type="text" name="message">
   <button>Post</button>
+  <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
 </form>
 
 <?php
