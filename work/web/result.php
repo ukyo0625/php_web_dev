1f5b7cc942f3cb331c83466a2a91784008cad389
kobayashiukyo <u.k.knua0419@gmail.com>
2020-10-10 19:28:09 +0900

    add_invalid_request

--- work/web/result.php
+++ work/web/result.php
@@ -2,13 +2,19 @@
 
 require('../app/functions.php');
 
-$message = trim(filter_input(INPUT_POST, 'message'));
-$message = $message !== '' ? $message : '...';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $message = trim(filter_input(INPUT_POST, 'message'));
+  $message = $message !== '' ? $message : '...';
 
-$filename = '../app/messages.txt';
-$fp = fopen($filename, 'a');
-fwrite($fp, $message . "\n");
-fclose($fp);
+  $filename = '../app/messages.txt';
+  $fp = fopen($filename, 'a');
+  fwrite($fp, $message . "\n");
+  fclose($fp);
+} else {
+  exit('Invalid Request');
+}
+
+
 
 include('../app/_parts/_header.php');
 ?>
